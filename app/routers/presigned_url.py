8c5194cc7a7b842edbed4e1c2211a5adc7708e28
root <root@ip-172-31-29-119.ap-southeast-1.compute.internal>
2022-10-25 09:52:08 +0000

    Add handling for duplicate obj in s3

--- app/routers/presigned_url.py
+++ app/routers/presigned_url.py
@@ -20,9 +20,7 @@ def generate_download_presigned_url(key: str, user: User = Depends(get_current_u
     Generate a presigned URL to retrieve a private object. User authentication required.
     """
     # check if key exist in s3
-    try:
-        s3_client.head_object(Bucket=CONFIG.S3_BUCKET_NAME, Key=key)
-    except s3_client.exceptions.ClientError:
+    if not check_key_exist_in_s3(key):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object not found")
 
     # generate presigned url
@@ -41,6 +39,10 @@ def generate_upload_presigned_url(req_dto: UploadPresignedUrlReqDto):
     curl --request PUT --url 'https://hataraki-dev-1.s3.amazonaws.com/myfile.png?AWSAccessKeyId=...&Signature=...&content-type=image%2Fpng&Expires=1666171105' \
     -H 'Content-Type: image/png' -T myfile.png
     """
+    # check if key exist in s3
+    if check_key_exist_in_s3(req_dto.key):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object already exist")
+
     try:
         presigned_url = s3_client.generate_presigned_url(
             ClientMethod="put_object",
@@ -55,3 +57,11 @@ def generate_upload_presigned_url(req_dto: UploadPresignedUrlReqDto):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
     return {"presignedUrl": presigned_url, "key": req_dto.key}
+
+
+def check_key_exist_in_s3(key):
+    try:
+        s3_client.head_object(Bucket=CONFIG.S3_BUCKET_NAME, Key=key)
+        return True
+    except s3_client.exceptions.ClientError:
+        return False
