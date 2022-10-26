ec5c06d5ced6d21a68081210165be8ed36b4e316
Don Tay <dontay0209@gmail.com>
2022-10-26 21:59:08 +0800

    Set fixture scopes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-import pytest_asyncio
+import pytest
 from beanie import init_beanie
 from httpx import AsyncClient
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -9,9 +9,16 @@ from app.models.job_listing import JobListing
 from app.models.user import User
 from app.server import app
 
+pytestmark = pytest.mark.anyio  # set anyio as the default test runner
 
-@pytest_asyncio.fixture
-async def client():
+
+@pytest.fixture(scope='session')
+def anyio_backend():
+    return 'asyncio'
+
+
+@pytest.fixture(scope='package')
+async def client(anyio_backend):
     db_client = AsyncIOMotorClient(CONFIG.MONGO_URI)
     await init_beanie(database=db_client.test, document_models=[JobApplication, JobListing, User])
     async with AsyncClient(app=app, base_url="http://test") as ac:
--- tests/routers/test_user.py
+++ tests/routers/test_user.py
@@ -4,21 +4,48 @@ from httpx import AsyncClient
 from app.models.user import User
 
 
-@pytest.mark.asyncio
-async def test_search_users(client: AsyncClient):
-    new_user = User(id="634cd28b6f3c8f9a18cf3d45", email="test@example.com",  # type: ignore
-                    company="test-company", password="test")
+@pytest.fixture(scope='module')
+async def loaded_client(client: AsyncClient):
+    new_user_1 = User(id="634cd28b6f3c8f9a18cf3d45", email="test@example.com",  # type: ignore
+                      company="test-company", password="test")
     try:
-        await new_user.save()
+        await new_user_1.save()
+        yield client
+    finally:
+        await User.delete_all()
 
-        response = await client.get("/api/v1/users")
+
+async def test_search_users_all(loaded_client: AsyncClient):
+    new_user_1 = User(id="634cd28b6f3c8f9a18cf3d45", email="test@example.com",  # type: ignore
+                      company="test-company", password="test")
+    response_1 = await loaded_client.get("/api/v1/users")
+    assert response_1.status_code == 200
+    res_json = response_1.json()
+    assert len(res_json) == 1
+    assert res_json[0]["email"] == new_user_1.email
+    assert res_json[0]["company"] == new_user_1.company
+    assert res_json[0]["roles"] == new_user_1.roles
+    assert res_json[0]["_id"] == str(new_user_1.id)
+
+
+@pytest.mark.parametrize(("query_params", "expected"),
+                         [("?email=does-not-exist@example.com", 0),
+                          ("?email=test@example.com", 1),
+                          ("?company=does-not-exist-co", 0),
+                          ("?company=test-company", 1),
+                          ("?company=test-company&email=test@example.com", 1),
+                          ("?company=test-company&email=does-not-exist@example.com", 0)
+                          ])
+async def test_search_users_query_params(loaded_client: AsyncClient, query_params: str, expected: int):
+    new_user_1 = User(id="634cd28b6f3c8f9a18cf3d45", email="test@example.com",  # type: ignore
+                      company="test-company", password="test")
+    try:
+        await new_user_1.save()
+
+        response = await loaded_client.get(f"/api/v1/users{query_params}")
         assert response.status_code == 200
         res_json = response.json()
-        assert len(res_json) == 1
-        assert res_json[0]["email"] == new_user.email
-        assert res_json[0]["company"] == new_user.company
-        assert res_json[0]["roles"] == new_user.roles
-        assert res_json[0]["_id"] == str(new_user.id)
+        assert len(res_json) == expected
 
     finally:
-        await new_user.delete()
+        await new_user_1.delete()
